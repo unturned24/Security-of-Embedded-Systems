7eb4e82c2c7852d51d4cc06ab296d99c1207bc20
Vaishnavi Balasubramanian <vaishiyer24@gmail.com>
2020-02-14 13:22:50 -0600

    Update tb_aes.v

--- AES-128/tb_aes.v
+++ AES-128/tb_aes.v
@@ -1,23 +1,23 @@
 `timescale 1ns / 1ps
 
-module tb_aes(clk,data_in,key,data_out);
-    input clk;
-    input [127:0] data_in;
-    input [127:0] key;
-    output [127:0] data_out;
-    reg clk;
+module AES_TB;
+	// Inputs
+	reg clk;
+	// Outputs
+	wire [7:0] final_out;
 
-aes_encrypt AE1(.clk(clk),.data_in(128'h 3243f6a8885a308d313198a2e0370734),.key(128'h 2b7e151628aed2a6abf7158809cf4f3c),.data_out(data_out));
+	// Instantiate the Unit Under Test (UUT)
+	aes_top dut (
+		.clk(clk), 
+		.final_out(final_out)
+	);
 
-initial begin
+	initial begin
 		// Initialize Inputs
 		clk = 0;
-
 		// Wait 100 ns for global reset to finish
 		#100;
-        
 		// Add stimulus here
-
 	end
-
+      
 endmodule

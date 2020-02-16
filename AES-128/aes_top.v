5d3ce2f82b958dabb0008bc2cf5e9fa87f1b910c
Vaishnavi Balasubramanian <vaishiyer24@gmail.com>
2020-02-16 17:00:28 -0600

    Update aes_top.v

--- AES-128/aes_top.v
+++ AES-128/aes_top.v
@@ -9,10 +9,8 @@ module aes_top(clk, data_out);
   output [127:0] data_out;
 
 //wire [127:0] tempout;
-  always @(posedge clk)
-  begin
+
 aes_encrypt u1(.clk(clk),.data_in(128'h 3243f6a8885a308d313198a2e0370734),.key(128'h 2b7e151628aed2a6abf7158809cf4f3c),.data_out(data_out));
-  end
-//assign final_out = tempout[127:120];
+ //assign final_out = tempout[127:120];
 
 endmodule

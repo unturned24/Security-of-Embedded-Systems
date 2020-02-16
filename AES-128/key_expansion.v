e563917ccb3fd908d9f51401a07d7a3ba6e8b10c
Vaishnavi Balasubramanian <vaishiyer24@gmail.com>
2020-02-16 16:12:54 -0600

    Update key_expansion.v

--- AES-128/key_expansion.v
+++ AES-128/key_expansion.v
@@ -4,7 +4,7 @@ module key_expansion(key, count, key_out);
     
   //declaration of i/ps and o/p
   input [127:0] key;
-    input [3:0] count;
+  input [3:0] count;
   output [127:0] key_out;
   
   //internal variables
@@ -14,9 +14,9 @@ module key_expansion(key, count, key_out);
   assign w3 = key[31:0];
     
   //calculating g(w3)
-  assign w_3 = {w3[23:16], w3[15:8], w3[7:0], w3[31:24]};
-  assign w3 = sub_byte(w_3);
-  assign temp = count;
+    assign w_3 = {w3[23:16], w3[15:8], w3[7:0], w3[31:24]};   //one byte left circular rotation
+    assign w3 = sub_byte(w_3);                               //substitute bytes using lookup table
+  assign temp = rcon;                                        //round constant
   assign key_out[127:96] = w0^w3^temp;
   assign key_out[95:64] = w1^w0^w3^temp;
   assign key_out[63:32] = w2^w1^w0^w3^temp;

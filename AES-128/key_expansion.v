28c12c5c6157748f08261c17fd6703797e492e11
Vaishnavi Balasubramanian <vaishiyer24@gmail.com>
2020-02-16 16:16:11 -0600

    Update key_expansion.v

--- AES-128/key_expansion.v
+++ AES-128/key_expansion.v
@@ -16,30 +16,30 @@ module key_expansion(key, count, key_out);
   //calculating g(w3)
     assign w_3 = {w3[23:16], w3[15:8], w3[7:0], w3[31:24]};   //one byte left circular rotation
     assign w3 = sub_byte(w_3);                               //substitute bytes using lookup table
-  assign temp = rcon;                                        //round constant
+     
+    case(count)	
+         4'h0: assign rcon=32'h01_00_00_00;
+         4'h1: assign rcon=32'h02_00_00_00;
+         4'h2: assign rcon=32'h04_00_00_00;
+         4'h3: assign rcon=32'h08_00_00_00;
+         4'h4: assign rcon=32'h10_00_00_00;
+         4'h5: assign assign rcon=32'h20_00_00_00;
+         4'h6: assign rcon=32'h40_00_00_00;
+         4'h7: assign assign rcon=32'h80_00_00_00;
+         4'h8: assign rcon=32'h1b_00_00_00;
+         4'h9: assign rcon=32'h36_00_00_00;
+         default: assign rcon=32'h00_00_00_00;
+       endcase
+
+    assign temp = rcon;                                        //round constant
   assign key_out[127:96] = w0^w3^temp;
   assign key_out[95:64] = w1^w0^w3^temp;
   assign key_out[63:32] = w2^w1^w0^w3^temp;
   assign key_out[95:64] = w3^w2^w1^w0^w3^temp;
   
   //To calculate round constant based on the round we're in 
-  function [32:0] rc;
-    input [3:0] count;
-    case(count)	
-         4'h0: rcon=32'h01_00_00_00;
-         4'h1: rcon=32'h02_00_00_00;
-         4'h2: rcon=32'h04_00_00_00;
-         4'h3: rcon=32'h08_00_00_00;
-         4'h4: rcon=32'h10_00_00_00;
-         4'h5: rcon=32'h20_00_00_00;
-         4'h6: rcon=32'h40_00_00_00;
-         4'h7: rcon=32'h80_00_00_00;
-         4'h8: rcon=32'h1b_00_00_00;
-         4'h9: rcon=32'h36_00_00_00;
-         default: rcon=32'h00_00_00_00;
-       endcase
 
-     endfunction
+
   
 endmodule
     

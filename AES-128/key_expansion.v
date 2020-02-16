4083bae7579aaa626e19e0e232b9ab19745b065f
Vaishnavi Balasubramanian <vaishiyer24@gmail.com>
2020-02-16 16:18:25 -0600

    Update key_expansion.v

--- AES-128/key_expansion.v
+++ AES-128/key_expansion.v
@@ -16,8 +16,8 @@ module key_expansion(key, count, key_out);
   //calculating g(w3)
     assign w_3 = {w3[23:16], w3[15:8], w3[7:0], w3[31:24]};   //one byte left circular rotation
     assign w3 = sub_byte(w_3);                               //substitute bytes using lookup table
-     
-    case(count)	
+    assign temp = count;
+    case(temp)	
          4'h0: assign rcon=32'h01_00_00_00;
          4'h1: assign rcon=32'h02_00_00_00;
          4'h2: assign rcon=32'h04_00_00_00;
@@ -30,12 +30,11 @@ module key_expansion(key, count, key_out);
          4'h9: assign rcon=32'h36_00_00_00;
          default: assign rcon=32'h00_00_00_00;
        endcase
-
-    assign temp = rcon;                                        //round constant
-  assign key_out[127:96] = w0^w3^temp;
-  assign key_out[95:64] = w1^w0^w3^temp;
-  assign key_out[63:32] = w2^w1^w0^w3^temp;
-  assign key_out[95:64] = w3^w2^w1^w0^w3^temp;
+                                      //round constant
+    assign key_out[127:96] = w0^w3^rcon;
+    assign key_out[95:64] = w1^w0^w3^rcon;
+    assign key_out[63:32] = w2^w1^w0^w3^rcon;
+    assign key_out[95:64] = w3^w2^w1^w0^w3^rcon;
   
   //To calculate round constant based on the round we're in 
 

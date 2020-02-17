0b5e97144718661b2bc93cbe4a85f544b9457863
Vaishnavi Balasubramanian <vaishiyer24@gmail.com>
2020-02-17 01:11:36 -0600

    Update tb_aes.v

--- AES-128/tb_aes.v
+++ AES-128/tb_aes.v
@@ -1,30 +1,67 @@
 `timescale 1ns / 1ps
-//`include "aes_encrypt.v"
+`define HalfClockPeriod 60
+`define ClockPeriod `HalfClockPeriod * 2
 
-module tb_aes;
-	// Inputs
-	//reg clk;
-	// Outputs
-	wire [7:0] data_out;
-	reg [127:0] data_in;
-	reg [127:0] key;
-	/*initial begin 
-    	clk =0;
-	end */
-	initial begin
-		#5;
-		data_in = 128'h 3243f6a8885a308d313198a2e0370734;
-		key = 128'h 2b7e151628aed2a6abf7158809cf4f3c;
-		#5;
-		$finish;
-	end
-	
-	// Instantiate the Unit Under Test (UUT)
-	/*always begin
-		#1 clk = ~clk;
-	end*/
-	
-	aes_encrypt u1(.data_in(data_in),.key(key),.data_out(data_out));
-	
-      
+module AES_tb;
+
+
+//INPUTS
+reg [127:0]data_in;
+reg [127:0]key;
+reg clk;
+reg reset;
+
+//OUTPUT
+wire [127:0] cryptokey;
+
+aes_top uut(
+            .data_in(data_in),
+            .key(key),
+            .clk(clk),
+            .reset(reset),
+            .cryptokey(cryptokey)
+            );
+            
+initial begin
+           //initialize input
+           data_in = 0;
+           key   = 0;
+           reset     = 1;
+           
+           //wait for global reset
+           #(1*`ClockPeriod);
+           
+           //TEST
+           #1
+           reset = 0;
+           data_in = 0;
+           key   = 0;
+           #(1*`ClockPeriod);
+           reset = 1;          
+           data_in = 128'h00112233445566778899aabbccddeeff; //00112233445566778899aabbccddeeff -> h0123456789abcdeffedcba9876543210
+           key   = 128'h000102030405060708090a0b0c0d0e0f; //0f1571c947d9e8590cb7add6af7f6798 - 000102030405060708090a0b0c0d0e0f
+           #(20*`ClockPeriod);
+           if(cryptokey == 128'h69c4e0d86a7b0430d8cdb78070b4c55a) //69c4e0d86a7b0430d8cdb78070b4c55a->ff0b844a0853bf7c6934ab4364148fb9
+            $display ("Test passed");
+           else     
+            $display ("failed: %x should be 128'h69c4e0d86a7b0430d8cdb78070b4c55a",cryptokey); 
+
+           
+//           #40
+//           if(encrypted_text == 128'h69c4e0d86a7b0430d8cdb78070b4c55a)
+//               $display ("Test passed");
+//           else     
+//               $display ("failed: %x should be 128'hff0b844a0853bf7c6934ab4364148fb9",encrypted_text); 
+             $stop;   
+end
+
+initial begin
+    clk = 0;
+end
+
+always begin
+    #`HalfClockPeriod clk = ~clk;
+    #`HalfClockPeriod clk = ~clk;
+   end
+
 endmodule

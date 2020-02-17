230d26c218e6c0e8dfd63fc9c8e56a673aee085b
Vaishnavi Balasubramanian <vaishiyer24@gmail.com>
2020-02-16 23:04:14 -0600

    Update tb_aes.v

--- AES-128/tb_aes.v
+++ AES-128/tb_aes.v
@@ -6,15 +6,25 @@ module tb_aes;
 	reg clk;
 	// Outputs
 	wire [7:0] data_out;
+	reg [127:0] data_in;
+	reg [127:0] key;
 	initial begin 
     	clk =0;
 	end
+	initial begin
+		#5;
+		data_in = 128'h 3243f6a8885a308d313198a2e0370734;
+		key = 128'h 2b7e151628aed2a6abf7158809cf4f3c;
+		#5;
+		$finish;
+	end
+	
 	// Instantiate the Unit Under Test (UUT)
 	always begin
 		#1 clk = ~clk;
 	end
 	
-	aes_encrypt u1(.clk(clk),.data_in(128'h 3243f6a8885a308d313198a2e0370734),.key(128'h 2b7e151628aed2a6abf7158809cf4f3c),.data_out(data_out));
+	aes_encrypt u1(.clk(clk),.data_in(data_in),.key(key),.data_out(data_out));
 	
       
 endmodule

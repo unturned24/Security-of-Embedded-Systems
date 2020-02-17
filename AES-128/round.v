3e32905ee1dc58cc045b42c603d3c878568c2228
Vaishnavi Balasubramanian <vaishiyer24@gmail.com>
2020-02-17 00:23:12 -0600

    Update round.v

--- AES-128/round.v
+++ AES-128/round.v
@@ -1,10 +1,10 @@
 `timescale 1ns / 1ps
 
 module round(data,key_in,out);
-input [3:0]rc;
+//input [3:0]rc;
 input [127:0]data;
 input [127:0]key_in;
-output [127:0]key_out;
+  output [127:0] out;
 //tput [127:0]rndout;
 wire [127:0] sb,sr,mc;
 //encrypts data for a given round taking round_count as input

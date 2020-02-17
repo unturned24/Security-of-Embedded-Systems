efcd619eb7a44a96da2e766bbfbd3423c2f23943
Vaishnavi Balasubramanian <vaishiyer24@gmail.com>
2020-02-17 11:43:55 -0600

    Update last_round.v

--- AES-128/last_round.v
+++ AES-128/last_round.v
@@ -10,8 +10,8 @@ output [127:0]data_out;
         
 //last round of operations doesn't involve mix_columns
 //key_expansion t0(rc,key_last,key_out);
-sub_byte t1(in,sb);
-shift_rows t2(sb,sr_out);
+  sub_byte t1(.in(in),.sb(sb));
+  shift_rows t2(.in(sb),.out(sr_out));
 assign sr = {sr_out[127:120],sr_out[95:88],sr_out[63:56],sr_out[31:24],sr_out[119:112],sr_out[87:80],sr_out[55:48],sr_out[23:16],sr_out[111:104],sr_out[79:72],sr_out[47:40],sr_out[15:8],sr_out[103:96],sr_out[71:64],sr_out[39:32],sr_out[7:0]};
 assign out = sr ^ mod_key;
 assign data_out = {out[127:120],out[95:88],out[63:56],out[31:24],out[119:112],out[87:80],out[55:48],out[23:16],out[111:104],out[79:72],out[47:40],out[15:8],out[103:96],out[71:64],out[39:32],out[7:0]};    

65fd4ed70d468d21139f031cc68708dc90cbf7c5
Vaishnavi Balasubramanian <vaishiyer24@gmail.com>
2020-02-17 11:45:24 -0600

    Update round.v

--- AES-128/round.v
+++ AES-128/round.v
@@ -9,8 +9,8 @@ input [127:0]key_in;
 wire [127:0] sb,sr,mc;
 //encrypts data for a given round taking round_count as input
 //y_expansion t0(rc,key_in,key_out);
-sub_byte t1(data,sb);
-shift_rows t2(sb,sr);
-mix_columns t3(sr,mc);
+  sub_byte t1(.in(data),.sb(sb));
+  shift_rows t2(.in(sb),.out(sr));
+  mix_columns t3(.in(sr),.out(mc));
 assign out = mc ^ key_in;
 endmodule

56a864122d1a9a33532a56bd2b847ac7626231a4
Vaishnavi Balasubramanian <vaishiyer24@gmail.com>
2020-02-16 22:38:41 -0600

    Update tb_aes.v

--- AES-128/tb_aes.v
+++ AES-128/tb_aes.v
@@ -1,19 +1,20 @@
 `timescale 1ns / 1ps
-//`include "aes_top.v"
+`include "aes_encrypt.v"
 
 module tb_aes;
 	// Inputs
 	reg clk;
 	// Outputs
-	wire [7:0] final_out;
+	wire [7:0] data_out;
 	initial begin 
-    		forever begin
-    		clk = 0;
-    		#1 clk = ~clk;
-		 end end
+    	clk =0;
+	end
 	// Instantiate the Unit Under Test (UUT)
-	aes_top dut ( .clk(clk), .data_out(final_out));
+	always begin
+		#1 clk = ~clk;
+	end
 	
+	aes_encrypt u1(.clk(clk),.data_in(128'h 3243f6a8885a308d313198a2e0370734),.key(128'h 2b7e151628aed2a6abf7158809cf4f3c),.data_out(data_out));
 	
       
 endmodule

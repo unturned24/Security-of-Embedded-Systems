fc1f563b8ea25e734840ca40bc4c13c6a2e00831
Vaishnavi Balasubramanian <vaishiyer24@gmail.com>
2020-02-17 19:09:44 -0600

    Update key_expansion.v

--- AES-128/key_expansion.v
+++ AES-128/key_expansion.v
@@ -27,16 +27,17 @@ module key_expansion(key, count, key_out);
        function [31:0]	rcon;
          input	[3:0]	count;
          case(count)	
-         4'h0: rcon=32'h01_00_00_00;
-         4'h1: rcon=32'h02_00_00_00;
-         4'h2: rcon=32'h04_00_00_00;
-         4'h3: rcon=32'h08_00_00_00;
-         4'h4: rcon=32'h10_00_00_00;
-         4'h5: rcon=32'h20_00_00_00;
-         4'h6: rcon=32'h40_00_00_00;
-         4'h7: rcon=32'h80_00_00_00;
-         4'h8: rcon=32'h1b_00_00_00;
-         4'h9: rcon=32'h36_00_00_00;
+         4'h1: rcon=32'h01_00_00_00;
+         4'h2: rcon=32'h02_00_00_00;
+         4'h3: rcon=32'h04_00_00_00;
+         4'h4: rcon=32'h08_00_00_00;
+         4'h5: rcon=32'h10_00_00_00;
+         4'h6: rcon=32'h20_00_00_00;
+         4'h7: rcon=32'h40_00_00_00;
+         4'h8: rcon=32'h80_00_00_00;
+         4'h9: rcon=32'h1b_00_00_00;
+         4'hA: rcon=32'h36_00_00_00;
+         4'hB: rcon=32'h6c_00_00_00;
          default: rcon=32'h00_00_00_00;
        endcase
 

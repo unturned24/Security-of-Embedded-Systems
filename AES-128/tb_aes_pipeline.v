044d23045900da1d16b24b7c8898ab47b9ba6b6a
Vaishnavi Balasubramanian <vaishiyer24@gmail.com>
2020-02-17 20:18:34 -0600

    Update tb_aes_pipeline.v

--- AES-128/tb_aes_pipeline.v
+++ AES-128/tb_aes_pipeline.v
@@ -40,9 +40,130 @@ initial begin
            
            #(1*`ClockPeriod);
            reset = 1;          
-           data_in = 128'h00112233445566778899aabbccddeeff; //00112233445566778899aabbccddeeff -> h0123456789abcdeffedcba9876543210
-           key   = 128'h000102030405060708090a0b0c0d0e0f; //0f1571c947d9e8590cb7add6af7f6798 - 000102030405060708090a0b0c0d0e0f
+           plaintext = 128'h0123456789abcdeffedcba9876543210; //00112233445566778899aabbccddeeff -> h0123456789abcdeffedcba9876543210
+           userkey   = 128'h0f1571c947d9e8590cb7add6af7f6798; //0f1571c947d9e8590cb7add6af7f6798 - 000102030405060708090a0b0c0d0e0f
+           //cryptokey = ff0b844a0853bf7c6934ab4364148fb9
+           
+           #(1*`ClockPeriod);
+           plaintext = 128'h00112233445566778899aabbccddeeff ; //00112233445566778899aabbccddeeff -> h0123456789abcdeffedcba9876543210
+           userkey   = 128'h000102030405060708090a0b0c0d0e0f ; //0f1571c947d9e8590cb7add6af7f6798 -> 000102030405060708090a0b0c0d0e0f
            //cryptokey = 69c4e0d86a7b0430d8cdb78070b4c55a
+           
+           #(1*`ClockPeriod);
+           plaintext = 128'h0123456789abcdeffedcba9876543210; //00112233445566778899aabbccddeeff -> h0123456789abcdeffedcba9876543210
+           userkey   = 128'h0f1571c947d9e8590cb7add6af7f6798 ; //0f1571c947d9e8590cb7add6af7f6798 -> 000102030405060708090a0b0c0d0e0f
+           //cryptokey = ff0b844a0853bf7c6934ab4364148fb9
+           
+           #(1*`ClockPeriod);
+           plaintext = 128'h00112233445566778899aabbccddeeff ; //00112233445566778899aabbccddeeff -> h0123456789abcdeffedcba9876543210
+           userkey   = 128'h000102030405060708090a0b0c0d0e0f ; //0f1571c947d9e8590cb7add6af7f6798 -> 000102030405060708090a0b0c0d0e0f
+           //cryptokey = 69c4e0d86a7b0430d8cdb78070b4c55a
+           
+           #(1*`ClockPeriod);
+           plaintext = 128'h0123456789abcdeffedcba9876543210 ; //00112233445566778899aabbccddeeff -> h0123456789abcdeffedcba9876543210
+           userkey   = 128'h0f1571c947d9e8590cb7add6af7f6798 ; //0f1571c947d9e8590cb7add6af7f6798 -> 000102030405060708090a0b0c0d0e0f
+           //cryptokey = 69c4e0d86a7b0430d8cdb78070b4c55a
+           
+           #(1*`ClockPeriod);
+           plaintext = 128'h00112233445566778899aabbccddeeff ; //00112233445566778899aabbccddeeff -> h0123456789abcdeffedcba9876543210
+           userkey   = 128'h000102030405060708090a0b0c0d0e0f ; //0f1571c947d9e8590cb7add6af7f6798 -> 000102030405060708090a0b0c0d0e0f
+           //cryptokey = 69c4e0d86a7b0430d8cdb78070b4c55a
+           
+           #(1*`ClockPeriod);
+           plaintext = 128'h0123456789abcdeffedcba9876543210 ; //00112233445566778899aabbccddeeff -> h0123456789abcdeffedcba9876543210
+           userkey   = 128'h0f1571c947d9e8590cb7add6af7f6798 ; //0f1571c947d9e8590cb7add6af7f6798 -> 000102030405060708090a0b0c0d0e0f
+           //cryptokey = 69c4e0d86a7b0430d8cdb78070b4c55a
+           
+           #(1*`ClockPeriod);
+           plaintext = 128'h00112233445566778899aabbccddeeff ; //00112233445566778899aabbccddeeff -> h0123456789abcdeffedcba9876543210
+           userkey   = 128'h000102030405060708090a0b0c0d0e0f ; //0f1571c947d9e8590cb7add6af7f6798 -> 000102030405060708090a0b0c0d0e0f
+           //cryptokey = 69c4e0d86a7b0430d8cdb78070b4c55a
+           
+           #(1*`ClockPeriod);
+           plaintext = 128'h0123456789abcdeffedcba9876543210 ; //00112233445566778899aabbccddeeff -> h0123456789abcdeffedcba9876543210
+           userkey   = 128'h0f1571c947d9e8590cb7add6af7f6798 ; //0f1571c947d9e8590cb7add6af7f6798 -> 000102030405060708090a0b0c0d0e0f
+           //cryptokey = 69c4e0d86a7b0430d8cdb78070b4c55a
+           
+           #(1*`ClockPeriod);
+           plaintext = 128'h00112233445566778899aabbccddeeff ; //00112233445566778899aabbccddeeff -> h0123456789abcdeffedcba9876543210
+           userkey   = 128'h000102030405060708090a0b0c0d0e0f ; //0f1571c947d9e8590cb7add6af7f6798 -> 000102030405060708090a0b0c0d0e0f
+           //cryptokey = 69c4e0d86a7b0430d8cdb78070b4c55a
+           
+           #(1*`ClockPeriod);
+           plaintext = 128'h0123456789abcdeffedcba9876543210 ; //00112233445566778899aabbccddeeff -> h0123456789abcdeffedcba9876543210
+           userkey   = 128'h0f1571c947d9e8590cb7add6af7f6798 ; //0f1571c947d9e8590cb7add6af7f6798 -> 000102030405060708090a0b0c0d0e0f
+           //cryptokey = 69c4e0d86a7b0430d8cdb78070b4c55a
+           
+           #(1*`ClockPeriod);
+           plaintext = 128'h00112233445566778899aabbccddeeff ; //00112233445566778899aabbccddeeff -> h0123456789abcdeffedcba9876543210
+           userkey   = 128'h000102030405060708090a0b0c0d0e0f ; //0f1571c947d9e8590cb7add6af7f6798 -> 000102030405060708090a0b0c0d0e0f
+           //cryptokey = 69c4e0d86a7b0430d8cdb78070b4c55a                                                                                                   
+           
+           #(1*`ClockPeriod);
+           if(cryptokey == 128'hff0b844a0853bf7c6934ab4364148fb9) //69c4e0d86a7b0430d8cdb78070b4c55a->ff0b844a0853bf7c6934ab4364148fb9
+            $display ("Test1 passed");
+           else     
+            $display ("failed: %x should be 128'hff0b844a0853bf7c6934ab4364148fb9",cryptokey); 
+           #(1*`ClockPeriod);
+           if(cryptokey == 128'h69c4e0d86a7b0430d8cdb78070b4c55a) //69c4e0d86a7b0430d8cdb78070b4c55a->ff0b844a0853bf7c6934ab4364148fb9
+            $display ("Test2 passed");
+           else     
+            $display ("failed: %x should be 128'hff0b844a0853bf7c6934ab4364148fb9",cryptokey);           
+           
+           #(1*`ClockPeriod);
+           if(cryptokey == 128'hff0b844a0853bf7c6934ab4364148fb9) //69c4e0d86a7b0430d8cdb78070b4c55a->ff0b844a0853bf7c6934ab4364148fb9
+                       $display ("Test3 passed");
+           else     
+            $display ("failed: %x should be 128'hff0b844a0853bf7c6934ab4364148fb9",cryptokey); 
+           #(1*`ClockPeriod);
+           if(cryptokey == 128'h69c4e0d86a7b0430d8cdb78070b4c55a) //69c4e0d86a7b0430d8cdb78070b4c55a->ff0b844a0853bf7c6934ab4364148fb9
+                       $display ("Test4 passed");
+           else     
+            $display ("failed: %x should be 128'hff0b844a0853bf7c6934ab4364148fb9",cryptokey);
+            
+           #(1*`ClockPeriod);
+           if(cryptokey == 128'hff0b844a0853bf7c6934ab4364148fb9) //69c4e0d86a7b0430d8cdb78070b4c55a->ff0b844a0853bf7c6934ab4364148fb9
+                       $display ("Test5 passed");
+           else     
+            $display ("failed: %x should be 128'hff0b844a0853bf7c6934ab4364148fb9",cryptokey); 
+           #(1*`ClockPeriod);
+           if(cryptokey == 128'h69c4e0d86a7b0430d8cdb78070b4c55a) //69c4e0d86a7b0430d8cdb78070b4c55a->ff0b844a0853bf7c6934ab4364148fb9
+                       $display ("Test6 passed");
+           else     
+            $display ("failed: %x should be 128'hff0b844a0853bf7c6934ab4364148fb9",cryptokey);
+            
+           #(1*`ClockPeriod);
+           if(cryptokey == 128'hff0b844a0853bf7c6934ab4364148fb9) //69c4e0d86a7b0430d8cdb78070b4c55a->ff0b844a0853bf7c6934ab4364148fb9
+                       $display ("Test7 passed");
+           else     
+            $display ("failed: %x should be 128'hff0b844a0853bf7c6934ab4364148fb9",cryptokey); 
+           #(1*`ClockPeriod);
+           if(cryptokey == 128'h69c4e0d86a7b0430d8cdb78070b4c55a) //69c4e0d86a7b0430d8cdb78070b4c55a->ff0b844a0853bf7c6934ab4364148fb9
+                       $display ("Test8 passed");
+           else     
+            $display ("failed: %x should be 128'hff0b844a0853bf7c6934ab4364148fb9",cryptokey);
+            
+           #(1*`ClockPeriod);
+           if(cryptokey == 128'hff0b844a0853bf7c6934ab4364148fb9) //69c4e0d86a7b0430d8cdb78070b4c55a->ff0b844a0853bf7c6934ab4364148fb9
+                       $display ("Test9 passed");
+           else     
+            $display ("failed: %x should be 128'hff0b844a0853bf7c6934ab4364148fb9",cryptokey); 
+           #(1*`ClockPeriod);
+           if(cryptokey == 128'h69c4e0d86a7b0430d8cdb78070b4c55a) //69c4e0d86a7b0430d8cdb78070b4c55a->ff0b844a0853bf7c6934ab4364148fb9
+                       $display ("Test10 passed");
+           else     
+            $display ("failed: %x should be 128'hff0b844a0853bf7c6934ab4364148fb9",cryptokey);
+            
+           #(1*`ClockPeriod);
+           if(cryptokey == 128'hff0b844a0853bf7c6934ab4364148fb9) //69c4e0d86a7b0430d8cdb78070b4c55a->ff0b844a0853bf7c6934ab4364148fb9
+                       $display ("Test11 passed");
+           else     
+            $display ("failed: %x should be 128'hff0b844a0853bf7c6934ab4364148fb9",cryptokey); 
+           #(1*`ClockPeriod);
+           if(cryptokey == 128'h69c4e0d86a7b0430d8cdb78070b4c55a) //69c4e0d86a7b0430d8cdb78070b4c55a->ff0b844a0853bf7c6934ab4364148fb9
+                       $display ("Test12 passed");
+           else     
+            $display ("failed: %x should be 128'hff0b844a0853bf7c6934ab4364148fb9",cryptokey); 
            
           /* #(1*`ClockPeriod);
            data_in = 128'h00112233445566778899aabbccddeeff ; //00112233445566778899aabbccddeeff -> h0123456789abcdeffedcba9876543210
@@ -65,13 +186,7 @@ initial begin
            //cryptokey = 69c4e0d86a7b0430d8cdb78070b4c55a
            */
           
-                                                                                                           
-           
-           #(1*`ClockPeriod);
-           if(cryptokey == 128'h69c4e0d86a7b0430d8cdb78070b4c55a) //69c4e0d86a7b0430d8cdb78070b4c55a->ff0b844a0853bf7c6934ab4364148fb9
-            $display ("Test1 passed");
-           else     
-            $display ("failed: %x should be 128'h69c4e0d86a7b0430d8cdb78070b4c55a",cryptokey); 
+        
           /* #(1*`ClockPeriod);
            if(cryptokey == 128'h69c4e0d86a7b0430d8cdb78070b4c55a) //69c4e0d86a7b0430d8cdb78070b4c55a->ff0b844a0853bf7c6934ab4364148fb9
             $display ("Test2 passed");

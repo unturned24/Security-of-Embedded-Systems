24987a63f450f8461256f56e75efc805c4977f0e
Vaishnavi Balasubramanian <vaishiyer24@gmail.com>
2020-02-17 19:56:10 -0600

    Update shift_rows.v

--- AES-128/shift_rows.v
+++ AES-128/shift_rows.v
@@ -24,10 +24,11 @@ assign          out[39:32] = in[39:32];
 assign          out[31:24] = in[127:120];
 assign          out[23:16] = in[87:80];
 assign          out[15:8] = in[47:40];
-   assign          out[7:0] = in[7:0];  */
-assign out[127:96] = {in[127:120],in[87:80],in[47:40],in[7:0]};
-    assign out[95:64]  = {in[95:88], in[55:48], in[15:8], in[103:96]};
-    assign out[63:32]  = {in[63:56], in[23:16], in[111:104], in[71:64]};
-    assign out[31:0]   = {in[31:24], in[119:112], in[79:72], in[39:32]};
+assign          out[7:0] = in[7:0];  */
+
+   assign out[127:96] = {in[127:120],in[87:80],in[47:40],in[7:0]};
+   assign out[95:64]  = {in[95:88], in[55:48], in[15:8], in[103:96]};
+   assign out[63:32]  = {in[63:56], in[23:16], in[111:104], in[71:64]};
+   assign out[31:0]   = {in[31:24], in[119:112], in[79:72], in[39:32]};
 
 endmodule

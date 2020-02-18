e04a8ad9b78b1c090e7293099927b3e43d08b491
Vaishnavi Balasubramanian <vaishiyer24@gmail.com>
2020-02-17 18:54:41 -0600

    Update key_expansion.v

--- AES-128/key_expansion.v
+++ AES-128/key_expansion.v
@@ -19,7 +19,7 @@ module key_expansion(key, count, key_out);
     sub_byte s0(.in(w_3[7:0]),.sb(temp[7:0]));
     sub_byte s1(.in(w_3[15:8]),.sb(temp[15:8]));
     sub_byte s2(.in(w_3[23:16]),.sb(temp[23:16]));
-    sub_byte s1(.in(w_3[31:24]), .sb(temp[31:24]));  
+    sub_byte s3(.in(w_3[31:24]), .sb(temp[31:24]));  
     assign key_out[127:96] = key[127:96]^temp^rcon(count);
     assign key_out[95:64] = key[95:64]^key_out[127:96];
     assign key_out[63:32] = key[63:32]^key_out[95:64];
